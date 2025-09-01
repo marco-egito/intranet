16c1eeef1f8afd4750fe636fadd9b5a7e24c9be2
marco-egito <aureliog.t.i@gmail.com>
2025-08-31 22:42:05 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/intranet/', // <-- Esta é a linha mais importante!
+  base: '/intranet/', // <-- Esta é a linha mais importante
 });

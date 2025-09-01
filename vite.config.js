c7b01277c0e259bf17bd31caa405d2cd507a48b8
marco-egito <aureliog.t.i@gmail.com>
2025-09-01 11:46:46 -0300

    Mudança de dist para docs

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,14 @@
 
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // 1. Aponta para a nova raiz do seu projeto de desenvolvimento.
-  // O Vite vai procurar o index.html dentro desta pasta.
   root: 'src',
-
+  base: '/intranet/',
   build: {
-    // 2. Especifica o diretório de saída para o build.
-    // O caminho '../dist' significa "volte uma pasta a partir da raiz ('src')
-    // e crie a pasta 'dist' lá".
-    outDir: '../dist',
+    // ALTERAÇÃO CRÍTICA AQUI:
+    // Vamos gerar os arquivos na pasta 'docs' em vez de 'dist'.
+    outDir: '../docs', 
     
-    // Opcional, mas recomendado: Limpa a pasta 'dist' antes de cada build.
     emptyOutDir: true,
   }
 });

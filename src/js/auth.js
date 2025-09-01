0c09a35279d187648fdd7959530fa79c476637f1
marco-egito <aureliog.t.i@gmail.com>
2025-09-01 09:31:50 -0300

    Update auth.js

--- src/js/auth.js
+++ src/js/auth.js
@@ -5,13 +5,13 @@ import { getFirestore, doc, getDoc } from "firebase/firestore";
 import { loadDashboard } from './dashboard.js';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCLeWW39nqxsdv1YD-CNa9RSTv05lGHJxM",
-    authDomain: "eupsico-agendamentos-d2048.firebaseapp.com",
-    databaseURL: "https://eupsico-agendamentos-d2048-default-rtdb.firebaseio.com",
-    projectId: "eupsico-agendamentos-d2048",
-    storageBucket: "eupsico-agendamentos-d2048.appspot.com",
-    messagingSenderId: "1041518416343",
-    appId: "1:1041518416343:web:3b972c212c52a59ad7bb92"
+  apiKey: "AIzaSyDJqPJjDDIGo7uRewh3pw1SQZOpMgQJs5M",
+  authDomain: "eupsico-agendamentos-d2048.firebaseapp.com",
+  databaseURL: "https://eupsico-agendamentos-d2048-default-rtdb.firebaseio.com",
+  projectId: "eupsico-agendamentos-d2048",
+  storageBucket: "eupsico-agendamentos-d2048.firebasestorage.app",
+  messagingSenderId: "1041518416343",
+  appId: "1:1041518416343:web:0a11c03c205b802ed7bb92"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);

36b300a3f8f7f39e5eb1cb19f65d046823e55ff8
marco-egito <aureliog.t.i@gmail.com>
2025-09-01 14:22:26 -0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,12 @@
 // --- CONFIGURAÇÃO DO FIREBASE ---
 const firebaseConfig = {
-    apiKey: "SUA_API_KEY_AQUI", // COLOQUE SUA API KEY AQUI
-    authDomain: "eupsico-agendamentos-d2048.firebaseapp.com",
-    databaseURL: "https://eupsico-agendamentos-d2048-default-rtdb.firebaseio.com",
-    projectId: "eupsico-agendamentos-d2048",
-    storageBucket: "eupsico-agendamentos-d2048.appspot.com",
-    messagingSenderId: "1041518416343",
-    appId: "1:1041518416343:web:3b972c212c52a59ad7bb92"
+        apiKey: "AIzaSyDJqPJjDDIGo7uRewh3pw1SQZOpMgQJs5M",
+        authDomain: "eupsico-agendamentos-d2048.firebaseapp.com",
+        databaseURL: "https://eupsico-agendamentos-d2048-default-rtdb.firebaseio.com",
+        projectId: "eupsico-agendamentos-d2048",
+        storageBucket: "eupsico-agendamentos-d2048.firebasestorage.app",
+        messagingSenderId: "1041518416343",
+        appId: "1:1041518416343:web:0a11c03c205b802ed7bb92"
 };
 
 firebase.initializeApp(firebaseConfig);

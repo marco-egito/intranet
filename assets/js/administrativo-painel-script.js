d66c60b4a6399cc241a453dd2e951879fe9b1fcc
marco-egito <128816469+marco-egito@users.noreply.github.com>
2025-09-04 18:48:43 -0300

    Update administrativo-painel-script.js

--- assets/js/administrativo-painel-script.js
+++ assets/js/administrativo-painel-script.js
@@ -2,15 +2,13 @@
 
 // Use a mesma configuração do Firebase do seu outro painel
 const firebaseConfig = {
-    const firebaseConfig = {
-    apiKey: "AIzaSyDJqPJjDDIGo7uRewh3pw1SQZOpMgQJs5M",
-    authDomain: "eupsico-agendamentos-d2048.firebaseapp.com",
-    databaseURL: "https://eupsico-agendamentos-d2048-default-rtdb.firebaseio.com",
-    projectId: "eupsico-agendamentos-d2048",
-    storageBucket: "eupsico-agendamentos-d2048.firebasestorage.app",
-    messagingSenderId: "1041518416343",
-    appId: "1:1041518416343:web:0a11c03c205b802ed7bb92"
-};
+        apiKey: "AIzaSyDJqPJjDDIGo7uRewh3pw1SQZOpMgQJs5M",
+        authDomain: "eupsico-agendamentos-d2048.firebaseapp.com",
+        databaseURL: "https://eupsico-agendamentos-d2048-default-rtdb.firebaseio.com",
+        projectId: "eupsico-agendamentos-d2048",
+        storageBucket: "eupsico-agendamentos-d2048.firebasestorage.app",
+        messagingSenderId: "1041518416343",
+        appId: "1:1041518416343:web:0a11c03c205b802ed7bb92"
 };
 
 firebase.initializeApp(firebaseConfig);

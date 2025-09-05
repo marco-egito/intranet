762de0c3305c3160e0304ce099665b6c7326b108
marco-egito <128816469+marco-egito@users.noreply.github.com>
2025-09-05 10:09:32 -0300

    Update supervisao-painel-script.js

--- assets/js/supervisao-painel-script.js
+++ assets/js/supervisao-painel-script.js
@@ -1,6 +1,12 @@
 // assets/js/supervisao-painel-script.js (Versão 1)
 const firebaseConfig = {
-    // COLE AQUI A SUA CONFIGURAÇÃO DO FIREBASE
+    apiKey: "AIzaSyDJqPJjDDIGo7uRewh3pw1SQZOpMgQJs5M",
+  authDomain: "eupsico-agendamentos-d2048.firebaseapp.com",
+  databaseURL: "https://eupsico-agendamentos-d2048-default-rtdb.firebaseio.com",
+  projectId: "eupsico-agendamentos-d2048",
+  storageBucket: "eupsico-agendamentos-d2048.firebasestorage.app",
+  messagingSenderId: "1041518416343",
+  appId: "1:1041518416343:web:0a11c03c205b802ed7bb92"
 };
 
 firebase.initializeApp(firebaseConfig);
